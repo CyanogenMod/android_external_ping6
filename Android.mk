497d0c6a3e7ecd7a08bbf4b0f02930f1d64f0b4c
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-05-04 22:04:35 +0200

    ping6: Use -fno-strict-aliasing for now
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- Android.mk
+++ Android.mk
@@ -4,4 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= ping6.c
 LOCAL_MODULE := ping6
 LOCAL_MODULE_TAGS := eng
+LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_CFLAGS += -fno-strict-aliasing
 include $(BUILD_EXECUTABLE)
