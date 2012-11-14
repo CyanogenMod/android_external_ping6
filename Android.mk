959c2543dd4335f2b19daa636ffde813043654f7
Lorenzo Colitti <lorenzo@google.com>
2012-11-14 12:28:36 +0900

    Include ping6 in userdebug builds.
    Also require that the user be root or have Internet
    permission, like we do for IPv4.
    
    Change-Id: I8ab8350885548ab83d5198adee912137b36d7a8d

--- Android.mk
+++ Android.mk
@@ -3,5 +3,5 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= ping6.c
 LOCAL_MODULE := ping6
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := debug
 include $(BUILD_EXECUTABLE)
